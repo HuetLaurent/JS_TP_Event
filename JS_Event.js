5f1235e512142713c93d6e195b1d00934377fbeb
HuetLaurent <118887843+HuetLaurent@users.noreply.github.com>
2023-01-16 23:57:59 +0100

    second commit

--- JS_Event.js
+++ JS_Event.js
@@ -20,21 +20,50 @@ btnAdd.onclick = function () {
 
 //fieldset du Boutton +
 
+let b = 0;
 const titre = document.getElementById("titreInput");
 const contenue = document.getElementById("contenueInput");
 
+/*const deleteText = (a) => {
+    a.onclick = function () {
+        const element1 = document.getElementsByClassName(`titre${b}`);
+        element1.remove();
+        const element2 = document.getElementsByClassName(`contenue${b}`);
+        element2.remove();
+        const element3 = document.getElementsByClassName(`btnDelete${b}`);
+        element3.remove();
+    };
+}*/
+
 btnDone.onclick = function () {
     //creation de nouveaux posts
-    let titre = document.createElement("h1");
-    document.body.appendChild(titre);
-    titre.textContent = titreInput.value;
-    let contenue = document.createElement("p");
-    document.body.appendChild(contenue);
-    contenue.textContent = contenueInput.value;
+    b++;
 
-    let titreAnchor = document.createElement("a");
-    document.body.appendChild("header");
-    titreAnchor.textContent = titre.value;
+    let div1 = document.createElement("div");
+    document.body.appendChild(div1);
+    div1.setAttribute("id",`div${b}`);
+
+    let titre = document.createElement("h1");
+    document.getElementById(`div${b}`).appendChild(titre);
+    titre.textContent = titreInput.value;
+    titre.classList.add(`titre${b}`);
+
+    let a1 = document.createElement("a");
+    document.getElementById("navBar").appendChild(a1);
+    a1.textContent = titreInput.value;
+    a1.href = `#div${b}`;
+
+    let contenue = document.createElement("p");
+    document.getElementById(`div${b}`).appendChild(contenue);
+    contenue.textContent = contenueInput.value;
+    contenue.classList.add(`contenue${b}`);
+
+    let btnDelete = document.createElement("button");
+    document.getElementById(`div${b}`).appendChild(btnDelete);
+    btnDelete.textContent = "X";
+    btnDelete.setAttribute("id",`btnDelete`);
+
+    //navbar
 
     //fermeture du fieldset
     if (a == 0) {
